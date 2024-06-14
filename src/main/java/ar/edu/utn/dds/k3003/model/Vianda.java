e0458b70b6b23ecbc8c104f31f17a083b7ebfc6d
Daniel <danibareiro1996@gmail.com>
2024-06-14 16:21:52 -0300

    Viandas completas

--- src/main/java/ar/edu/utn/dds/k3003/model/Vianda.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Vianda.java
@@ -4,6 +4,7 @@ import ar.edu.utn.dds.k3003.facades.dtos.EstadoViandaEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -19,6 +20,7 @@ public class Vianda {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+  @Column(unique = true)
   private String codigoQR;
   private long colaboradorId;
   private Integer heladeraId;
